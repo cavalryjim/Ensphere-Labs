7b94b12f94a9491cde12b8a6bfad4c7187b11bbd
James Davis <james.davisphd@gmail.com>
2011-05-03 12:30:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnsphereLabs::Application.config.secret_token = '4831d4b1f93c6e7a33711a5688094bcf1533e6b1e90e2f104cdb53b768c4a53edcad15d8f009415c5d62108e82e2ffc232b5a356f2e95146c24de81a4c6fca74'
