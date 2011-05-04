1f673f9fe751af98f6891126fc19a81fb8472442
James Davis <james.davisphd@gmail.com>
2011-05-04 08:48:31 -0500

    test commit using EGit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def home
+  end
+
+  def portfolio
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 EnsphereLabs::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  root :to => "welcome#home"
+  
+  get "welcome/home"
+  get "welcome/portfolio"
+  get "welcome/about"
+  get "welcome/contact"
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -54,5 +60,5 @@ EnsphereLabs::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
